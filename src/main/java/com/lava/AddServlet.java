fea1534c78aa335ec0c67f06f262a6f16489c96e
Lavanya <slavanya2509@gmail.com>
2025-01-29 00:30:09 +0530

    Day 2

--- src/main/java/com/lava/AddServlet.java
+++ src/main/java/com/lava/AddServlet.java
@@ -3,17 +3,22 @@ package com.lava;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet
 {
-	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
 		int i=Integer.parseInt(req.getParameter("n1"));
 		int j=Integer.parseInt(req.getParameter("n2"));
 		int k=i+j;
 		
+		req.setAttribute("k", k);
+		RequestDispatcher rd=req.getRequestDispatcher("sq");
+		rd.forward(req, res);
 		PrintWriter out=res.getWriter();
 //		To print in the webpage
 		out.println(k);
